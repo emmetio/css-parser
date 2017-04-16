dee6ec81dcd0755e77df5695d9587ee142319662
Sergey Chikuyonok <serge.che@gmail.com>
2017-04-16 16:32:54 +0300

    Initial implementation

---
+++ index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+import StreamReader from '@emmetio/stream-reader';
+import { isSpace } from '@emmetio/stream-reader-utils';
+import Node from './lib/node';
+import createToken from './lib/token';
+import eatComment from './lib/parser/comment';
+import eatIdent from './lib/parser/identifier';
+import {
+	isSectionStart, isSectionEnd,
+	isPropertySeparator, isPropertyTerminator
+} from './lib/parser/separator';
+
+/**
+ * Parses given CSS (or stylesheet) code into a very simple and minimalistic
+ * object model
+ * @param  {StreamReader|String} source
+ * @return {Node}
+ */
+export default function parseCSS(source) {
+	const stream = typeof source === 'string' ? new StreamReader(source) : source;
+	const root = new Node('root');
+	let ctx = root, child, ch;
+
+	/** @type {Token} */
+	let ident, name;
+
+	while (!stream.eof()) {
+		if (stream.eatWhile(isSpace) || eatComment(stream)) {
+			continue;
+		}
+
+		ch = stream.peek();
+					if (eatIdent(stream)) {
+			if (ch === 64 /* @ */) {
+				// Edge case for some CSS properties like `@import`:
+				// at-rules always starts a new property or section
+				name = createToken(stream);
+				ident = null;
+			} else {
+				ident = createToken(stream, ident && ident.start);
+			}
+		} else if (isPropertySeparator(ch)) {
+			// Could be a property separator or a part of selector
+			if (name) {
+				// Already splitted by `:`, looks like pseudo-selector
+				name = createToken(stream, name.start, ident ? ident.end : name.end);
+			} else if (ident) {
+				name = ident;
+			} else {
+				// No consumed data before `:`, it might be a special CSS
+				// selector like `::slotted()`. Should create an empty name
+				// pointer
+				name = createToken(stream, stream.pos);
+			}
+			ident = null;
+			stream.next();
+		} else if (isPropertyTerminator(ch)) {
+			// data consumed before is a CSS property, create node for it
+			stream.start = stream.pos;
+			stream.next();
+			ctx.addChild(createProperty(name, ident, createToken(stream)));
+			name = ident = null;
+		} else if (isSectionStart(ch)) {
+			// Data consumed before is a CSS selector, create node for it
+			if (name && ident) {
+				// Seems like property-value pair, combine it into a single selector
+				name = createToken(stream, name.start, ident.end);
+			} else if (ident) {
+				name = ident;
+			} else {
+				// Something weird: no section name. Create empty stub for it
+				name = createToken(stream, stream.pos);
+			}
+
+			stream.next();
+			child = new Node('section', name, createToken(stream, stream.pos));
+			ctx.addChild(child);
+			ctx = child;
+			name = ident = child = null;
+		} else if (isSectionEnd(ch)) {
+			// Finalize context section
+			if (name || ident) {
+				// There’s pending CSS property
+				ctx.addChild(createProperty(name, ident));
+			}
+
+			stream.start = stream.pos;
+			stream.next();
+			ident = createToken(stream);
+
+			if (ctx.type !== 'root') {
+				// In case of invalid stylesheet with redundant `}`,
+				// don’t modify root section.
+				ctx._value = createToken(stream, ctx._value.start, stream.start);
+				ctx._terminator = createToken(stream);
+			}
+
+			ctx = ctx.parent || ctx;
+			name = ident = child = null;
+		} else {
+			throw new Error(`Unexpected ${String.fromCharCode(ch)} at ${stream.pos}`);
+		}
+	}
+
+	if (name || ident) {
+		// There’s pending CSS property
+		ctx.addChild(createProperty(name, ident));
+	}
+
+	return root;
+}
+
+function createProperty(name, value, terminator) {
+	return new Node('property', name || value, name ? value : null, terminator);
+}
---
+++ test/parse.js
@@ -0,0 +1,133 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+require('babel-register');
+const parse = require('../index').default;
+
+describe('CSS Parser', () => {
+	const readFile = file => fs.readFileSync(path.resolve(__dirname, file), 'utf8');
+
+	it('should parse properties', () => {
+		const model = parse(readFile('./fixtures/properties.css'));
+		let node;
+
+		assert.equal(model.children.length, 5);
+
+		node = model.children[0];
+		assert.equal(node.name, '@a');
+		assert.equal(node.value, '10');
+		assert.equal(node.terminator, ';');
+		assert.equal(node.children.length, 0);
+
+		node = model.children[1];
+		assert.equal(node.name, '$bc');
+		assert.equal(node.value, 'foo');
+		assert.equal(node.terminator, ';');
+		assert.equal(node.type, 'property');
+		assert.equal(node.children.length, 0);
+
+		node = model.children[2];
+		assert.equal(node.name, '@import');
+		assert.equal(node.value, '"url"');
+		assert.equal(node.terminator, ';');
+		assert.equal(node.type, 'property');
+		assert.equal(node.children.length, 0);
+
+		node = model.children[3];
+		assert.equal(node.name, '@import');
+		assert.equal(node.value, '"a", "b"');
+		assert.equal(node.terminator, ';');
+		assert.equal(node.type, 'property');
+		assert.equal(node.children.length, 0);
+
+		node = model.children[4];
+		assert.equal(node.name, '@import');
+		assert.equal(node.value, 'url("foo bar")');
+		assert.equal(node.terminator, '');
+		assert.equal(node.type, 'property');
+		assert.equal(node.children.length, 0);
+	});
+
+	it('should parse sections', () => {
+		const model = parse(readFile('./fixtures/sections.css'));
+		let node;
+
+		assert.equal(model.children.length, 3);
+
+		node = model.children[0];
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, 'body');
+		assert.equal(node.terminator, '}');
+		assert.equal(node.children.length, 2);
+
+		assert.equal(node.children[0].type, 'property');
+		assert.equal(node.children[0].name, 'padding');
+		assert.equal(node.children[0].value, '10px');
+
+		assert.equal(node.children[1].type, 'section');
+		assert.equal(node.children[1].name, '&:hover &::before');
+
+		node = model.children[1];
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, '@media print');
+		assert.equal(node.terminator, '}');
+		assert.equal(node.children.length, 1);
+
+		assert.equal(node.children[0].type, 'section');
+		assert.equal(node.children[0].name, 'a[foo="b:a;r"]::before');
+		assert.equal(node.children[0].children.length, 1);
+
+		// test complex selector with LESS-style mixins
+		node = node.children[0].firstChild;
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, '::slotted(.foo)');
+		assert.equal(node.children.length, 3);
+
+		assert.equal(node.children[0].type, 'property');
+		assert.equal(node.children[0].name, '.foo.bar');
+		assert.equal(node.children[0].value, null);
+
+		assert.equal(node.children[1].type, 'property');
+		assert.equal(node.children[1].name, 'a');
+		assert.equal(node.children[1].value, 'b');
+
+		assert.equal(node.children[2].type, 'property');
+		assert.equal(node.children[2].name, '#baz.bar');
+		assert.equal(node.children[2].value, null);
+		assert.equal(node.children[2].terminator, '');
+
+		// test multiline sector
+		node = model.children[2];
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, '.foo,\n#bar');
+		assert.equal(node.children.length, 1);
+
+		assert.equal(node.children[0].type, 'property');
+		assert.equal(node.children[0].name, 'margin');
+		assert.equal(node.children[0].value, 'auto');
+	});
+
+	it('should handle comments', () => {
+		const model = parse(readFile('./fixtures/comments.scss'));
+		let node;
+
+		assert.equal(model.children.length, 1);
+
+		node = model.firstChild;
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, 'foo /* a:b; */, bar');
+		assert.equal(node.children.length, 1);
+
+		node = node.firstChild;
+		assert.equal(node.type, 'section');
+		assert.equal(node.name, 'a, // c:b {}\n\tb');
+		assert.equal(node.children.length, 1);
+
+		node = node.firstChild;
+		assert.equal(node.type, 'property');
+		assert.equal(node.name, 'padding');
+		assert.equal(node.value, '0');
+	});
+});
