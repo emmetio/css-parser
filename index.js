83f5709f3802efbaf4c2327338129d96c779027c
Sergey Chikuyonok <serge.che@gmail.com>
2017-04-27 02:04:38 +0300

    Working on performance improvements

--- index.js
+++ index.js
@@ -4,56 +4,96 @@ import StreamReader from '@emmetio/stream-reader';
 import Stylesheet from './lib/stylesheet';
 import createRule from './lib/rule';
 import createProperty from './lib/property';
-import token, { unknown, Token } from './lib/tokens/index';
+import { Token } from './lib/tokens/index';
 
-export default function(source) {
+import atKeyword from './lib/tokens/at-keyword';
+import string from './lib/tokens/string';
+import separator from './lib/tokens/separator';
+import comment, { multiLineComment } from './lib/tokens/comment';
+import whitespace from './lib/tokens/whitespace';
+
+const LBRACE = 40;  // (
+const RBRACE = 41;  // )
+
+export default function parseStylesheet(source) {
 	const stream = typeof source === 'string' ? new StreamReader(source) : source;
 	const root = new Stylesheet();
+	let ctx = root, child, accum;
+	let token, start;
 	let tokens = [];
-	let ctx = root, child, t;
 
 	while (!stream.eof()) {
-		t = token(stream);
+		token = atKeyword(stream) || separator(stream) || whitespace(stream) || comment(stream);
+		if (token) {
+			if (accum) {
+				tokens.push(accum);
+				accum = null;
+			}
 
-		if (!t) {
-			// unable to identify following character, consume it as unknown token
-			stream.start = stream.pos;
-			stream.next();
-			tokens.push(unknown(stream));
-		} else if (t.propertyTerminator) {
-			// Tokens consumed before are CSS property
-			tokens.push(t);
-			ctx.addChild(createProperty(stream, tokens, t));
-			tokens = [];
-		} else if (t.ruleStart) {
-			// Tokens consumed before are CSS rule
-			child = createRule(stream, tokens, t);
-			if (child) {
+			if (token.propertyTerminator) {
+				ctx.addChild(createProperty(stream, tokens, token));
+				tokens = [];
+			} else if (token.ruleStart) {
+				child = createRule(stream, tokens, token);
 				ctx.addChild(child);
 				ctx = child;
-			}
-			tokens = [];
-		} else if (t.ruleEnd) {
-			// Finalize previously consumed tokens as CSS property
-			ctx.addChild(createProperty(stream, tokens));
-			tokens = [];
+				tokens = [];
+			} else if (token.ruleEnd) {
+				// Finalize context section
+				ctx.addChild(createProperty(stream, tokens));
 
-			// In case of invalid stylesheet with redundant `}`,
-			// don’t modify root section.
-			if (ctx.type !== 'root') {
-				ctx.contentEnd = t;
+				if (ctx.type !== 'root') {
+					// In case of invalid stylesheet with redundant `}`,
+					// don’t modify root section.
+					ctx.contentRange.end = token.end;
+					ctx = ctx.parent;
+				}
+
+				tokens = [];
+			} else {
+				tokens.push(token);
 			}
 
-			ctx = ctx.parent || ctx;
+			continue;
+		}
+
+		start = stream.pos;
+		if (braces(stream) || string(stream) || !isNaN(stream.next())) {
+			if (!accum) {
+				accum = new Token(stream, start);
+			} else {
+				accum.end = stream.pos;
+			}
 		} else {
-			tokens.push(t);
+			throw new Error(`Unexpected end-of-stream at ${stream.pos}`);
 		}
 	}
 
-	// save unterminated tokens as property
+	// Finalize all the rest properties
 	ctx.addChild(createProperty(stream, tokens));
 
 	return root;
 }
 
-export { token, createProperty, createRule, Token }
+function braces(stream) {
+	if (stream.eat(LBRACE)) {
+		let stack = 1;
+
+		// Handle edge case: do not consume single-line comment inside braces
+		// since most likely it’s an unquoted url like `http://example.com`
+		while (!stream.eof()) {
+			if (stream.eat(RBRACE)) {
+				stack--;
+				if (!stack) {
+					break;
+				}
+			} else if (!string(stream) && !multiLineComment(stream)) {
+				stream.next();
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+}
