c7f3a0c6a7d937b6939ddca0d7d04f707de1b1bf
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-02 12:09:26 +0300

    Slight performance improvement

--- index.js
+++ index.js
@@ -6,21 +6,30 @@ import createRule from './lib/rule';
 import createProperty from './lib/property';
 
 import atKeyword from './lib/tokens/at-keyword';
-import separator from './lib/tokens/separator';
 import { eatString } from './lib/tokens/string';
 import { eatComment } from './lib/tokens/comment';
 import { eatWhitespace } from './lib/tokens/whitespace';
 import { eatUrl } from './lib/tokens/url';
 import { Token } from './lib/tokens/index';
 
-const LBRACE = 40;  // (
-const RBRACE = 41;  // )
+const LBRACE          = 40;  // (
+const RBRACE          = 41;  // )
+const PROP_DELIMITER  = 58;  // :
+const PROP_TERMINATOR = 59;  // ;
+const RULE_START      = 123; // {
+const RULE_END        = 125; // }
 
 export default function parseStylesheet(source) {
 	const stream = typeof source === 'string' ? new StreamReader(source) : source;
 	const root = new Stylesheet();
 	let ctx = root, child, accum, token;
 	let tokens = [];
+	const flush = () => {
+		if (accum) {
+			tokens.push(accum);
+			accum = null;
+		}
+	};
 
 	while (!stream.eof()) {
 		if (eatWhitespace(stream) || eatComment(stream)) {
@@ -29,48 +38,52 @@ export default function parseStylesheet(source) {
 
 		stream.start = stream.pos;
 
-		if (token = separator(stream)) {
-			// Consumed separator, create either rule or property from it
-			accum && tokens.push(accum);
-			accum = null;
-
-			switch (token.property('type')) {
-				case 'propertyTerminator':
-					ctx.addChild(createProperty(stream, tokens, token));
-					tokens.length = 0;
-					break;
-
-				case 'ruleStart':
-					child = createRule(stream, tokens, token);
-					ctx.addChild(child);
-					ctx = child;
-					tokens.length = 0;
-					break;
-
-				case 'ruleEnd':
-					// Finalize context section
-					ctx.addChild(createProperty(stream, tokens));
-
-					if (ctx.type !== 'stylesheet') {
-						// In case of invalid stylesheet with redundant `}`,
-						// don’t modify root section.
-						ctx.contentToken.end = token.end;
-						ctx = ctx.parent;
-					}
-
-					tokens.length = 0;
-					break;
-
-				default:
-					tokens.push(token);
+		if (stream.eatWhile(PROP_DELIMITER)) {
+			// Property delimiter can be either a real property delimiter or a
+			// part of pseudo-selector.
+			if (!tokens.length) {
+				if (accum) {
+					// No consumed tokens yet but pending token: most likely it’s
+					// a CSS property
+					flush();
+				} else {
+					// No consumend or accumulated token, seems like a start of
+					// pseudo-selector, e.g. `::slotted`
+					accum = new Token(stream, 'preparse');
+				}
 			}
+			// Skip delimiter if there are already consumend tokens: most likely
+			// it’s a part of pseudo-selector
+		} else if (stream.eat(RULE_END)) {
+			flush();
+
+			// Finalize context section
+			ctx.addChild(createProperty(stream, tokens));
+
+			if (ctx.type !== 'stylesheet') {
+				// In case of invalid stylesheet with redundant `}`,
+				// don’t modify root section.
+				ctx.contentToken.end = stream.pos;
+				ctx = ctx.parent;
+			}
+
+			tokens.length = 0;
+		} else if (stream.eat(PROP_TERMINATOR)) {
+			flush();
+			ctx.addChild(createProperty(stream, tokens, new Token(stream, 'termintator')));
+			tokens.length = 0;
+		} else if (stream.eat(RULE_START)) {
+			flush();
+			child = createRule(stream, tokens, new Token(stream, 'body'));
+			ctx.addChild(child);
+			ctx = child;
+			tokens.length = 0;
 		} else if (token = atKeyword(stream)) {
 			// Explictly consume @-tokens since it defines how rule or property
 			// should be pre-parsed
-			accum && tokens.push(accum);
-			accum = null;
+			flush();
 			tokens.push(token);
-		} else if (eatUrl(stream) || eatBraces(stream) || eatString(stream) || !isNaN(stream.next())) {
+		} else if (eatUrl(stream) || eatBraces(stream) || eatString(stream) || stream.next()) {
 			// NB explicitly consume `url()` token since it may contain
 			// an unquoted url like `http://example.com` which interferes
 			// with single-line comment
@@ -93,7 +106,7 @@ export default function parseStylesheet(source) {
 
 /**
  * Consumes content inside round braces. Mostly used to skip `;` token inside
- * since in LESS it is also used to separate function arguments
+ * expressions since in LESS it is also used to separate function arguments
  * @param  {StringReader} stream
  * @return {Boolean}
  */
