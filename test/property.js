3942002325c21b4efa48732839fa609ea755417a
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-04 01:13:37 +0300

    Support detailed property parser

--- test/property.js
+++ test/property.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 require('babel-register');
 const parse = require('../index').default;
 
-describe.skip('Property Fragments', () => {
+describe('Property Fragments', () => {
 	const parseProperty = prop => parse(prop).firstChild;
 	it('should parse simple property', () => {
 		const prop = parseProperty('padding: 10px 20% ;');
@@ -14,19 +14,25 @@ describe.skip('Property Fragments', () => {
 		assert.equal(prop.name, 'padding');
 
 		assert.equal(prop.value, '10px 20%');
-		assert.equal(prop.valueToken.type, 'fragments');
+		assert.equal(prop.parsedValue.length, 1);
 
-		const f = prop.valueToken.fragments;
-		assert.equal(f[0].type, 'fragments');
-		assert.equal(f[0].fragments[0].type, 'number');
-		assert.equal(f[0].fragments[0].number.valueOf(), '10');
-		assert.equal(f[0].fragments[0].unit.valueOf(), 'px');
+		const item = prop.parsedValue[0];
+		assert.equal(item.type, 'item');
+		assert.equal(item.size, 3);
 
-		assert.equal(f[0].fragments[1].type, 'whitespace');
+		let sub = item.item(0);
+		assert.equal(sub.type, 'number');
+		assert.equal(sub.valueOf(), '10px');
+		assert.equal(sub.item(0).valueOf(), '10');
+		assert.equal(sub.item(1).valueOf(), 'px');
 
-		assert.equal(f[0].fragments[2].type, 'number');
-		assert.equal(f[0].fragments[2].number.valueOf(), '20');
-		assert.equal(f[0].fragments[2].unit.valueOf(), '%');
+		assert.equal(item.item(1).type, 'whitespace');
+
+		sub = item.item(2);
+		assert.equal(sub.type, 'number');
+		assert.equal(sub.valueOf(), '20%');
+		assert.equal(sub.item(0).valueOf(), '20');
+		assert.equal(sub.item(1).valueOf(), '%');
 	});
 
 	it('should parse URL', () => {
@@ -35,39 +41,53 @@ describe.skip('Property Fragments', () => {
 		assert.equal(prop.name, 'background');
 		assert.equal(prop.value, 'url(http://example.com/image.jpg)');
 
-		const url = prop.valueToken.fragments[0].fragments[0];
+		const url = prop.parsedValue[0].item(0);
 
 		assert.equal(url.type, 'url');
 		assert.equal(url.valueOf(), 'url(http://example.com/image.jpg)');
-		assert.equal(url.value.valueOf(), 'http://example.com/image.jpg');
+		assert.equal(url.item(0).valueOf(), 'http://example.com/image.jpg');
 	});
 
 	it('should parse multiple values', () => {
-		const prop = parseProperty('foo: rgb(1, 2, 3), 10px, bar(baz())');
+		const prop = parseProperty('foo: rgb(1, 2, 3), 10px 20%, bar(baz())');
 
 		assert.equal(prop.name, 'foo');
-		assert.equal(prop.value, 'rgb(1, 2, 3), 10px, bar(baz())');
+		assert.equal(prop.value, 'rgb(1, 2, 3), 10px 20%, bar(baz())');
 
-		const parts = prop.valueToken.fragments;
+		assert.equal(prop.parsedValue.length, 3);
 
-		assert(parts[0].fragments[0].type, 'function');
-		assert(parts[0].fragments[0].name.valueOf(), 'rgb');
-		assert(parts[0].fragments[0].arguments.list.length, 3);
-		assert(parts[0].fragments[0].arguments.list[0].valueOf(), '1');
-		assert(parts[0].fragments[0].arguments.list[1].valueOf(), '2');
-		assert(parts[0].fragments[0].arguments.list[2].valueOf(), '3');
+		let part = prop.parsedValue[0];
+		let item = part.item(0);
+		assert.equal(item.type, 'function');
+		assert.equal(item.item(0).valueOf(), 'rgb', 'Function name');
+		assert.equal(item.item(1).type, 'arguments', 'Function arguments');
+		assert.equal(item.item(1).size, 3);
+		assert.equal(item.item(1).item(0).valueOf(), '1');
+		assert.equal(item.item(1).item(1).valueOf(), '2');
+		assert.equal(item.item(1).item(2).valueOf(), '3');
 
-		assert(parts[1].fragments[0].type, 'number');
-		assert(parts[1].fragments[0].number.valueOf(), '10');
-		assert(parts[1].fragments[0].unit.valueOf(), 'px');
+		part = prop.parsedValue[1];
+		assert.equal(part.size, 3);
 
-		assert(parts[2].fragments[0].type, 'function');
-		assert(parts[2].fragments[0].name.valueOf(), 'bar');
-		assert(parts[2].fragments[0].arguments.list.length, 1);
+		item = part.item(0);
+		assert.equal(item.type, 'number');
+		assert.equal(item.item(0).valueOf(), '10');
+		assert.equal(item.item(1).valueOf(), 'px');
 
-		const innerFn = parts[2].fragments[0].arguments.list[0].fragments[0];
-		assert(innerFn.type, 'function');
-		assert(innerFn.name.valueOf(), 'baz');
-		assert(innerFn.valueOf(), 'baz()');
+		item = part.item(2);
+		assert.equal(item.type, 'number');
+		assert.equal(item.item(0).valueOf(), '20');
+		assert.equal(item.item(1).valueOf(), '%');
+
+		part = prop.parsedValue[2];
+		item = part.item(0);
+		assert.equal(item.type, 'function');
+		assert.equal(item.item(0).valueOf(), 'bar');
+		assert.equal(item.item(1).size, 1);
+
+		const innerFn = item.item(1).item(0).item(0);
+		assert.equal(innerFn.type, 'function');
+		assert.equal(innerFn.item(0).valueOf(), 'baz');
+		assert.equal(innerFn.valueOf(), 'baz()');
 	});
 });
