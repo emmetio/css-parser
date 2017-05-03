40e30a7675015a63f1cd9d58441c4f0379e29e35
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-04 01:17:22 +0300

    Prepare for release

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,15 @@ import resolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: './index.js',
-	// external: [
-	// 	'@emmetio/stream-reader',
-	// 	'@emmetio/stream-reader-utils'
-	// ],
+	external: [
+		'@emmetio/stream-reader',
+		'@emmetio/stream-reader-utils'
+	],
 	exports: 'named',
 	moduleName: 'cssParser',
 	plugins: [resolve()],
 	targets: [
 		{format: 'cjs', dest: 'dist/css-parser.cjs.js'},
-		{format: 'es',  dest: 'dist/css-parser.es.js'},
-		{format: 'iife',  dest: 'dist/css-parser.js'}
+		{format: 'es',  dest: 'dist/css-parser.es.js'}
 	]
 };
